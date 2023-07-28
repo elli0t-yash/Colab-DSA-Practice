64f467028679a1f8685029966b7fca7fb3d11bb9
Yash Mishra <yash01012002@gmail.com>
2023-07-29 00:51:53 +0530

    quicksort

--- yash/Sorting/sort.cpp
+++ yash/Sorting/sort.cpp
@@ -51,6 +51,28 @@ void selectionSort(int arr[], int n) {
     
 }
 
+// Quicksort
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+    for(int j = low; j <= high - 1; j++) {
+        if(arr[j] < pivot) {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return(i+1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if(low < high) {
+        int partition_index = partition(arr, low, high);
+        quickSort(arr, low, partition_index - 1);
+        quickSort(arr, high, partition_index + 1);
+    }
+}
+
 int main(){
     int n;
     cin>>n;
@@ -59,7 +81,8 @@ int main(){
         cin>>arr[i];
     }
     // bubbleSort(arr,n);
-    insertionSort(arr,n);
+    // insertionSort(arr,n);
+    quickSort(arr,0,n-1);
     for (int i = 0; i < n; i++) {
         cout<<arr[i];
     }
