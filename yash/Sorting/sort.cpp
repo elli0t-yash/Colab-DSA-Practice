a11354bc6ec1b2f3ebf85730872efb4301f67cf5
Yash Mishra <yash01012002@gmail.com>
2023-07-29 13:08:07 +0530

    heap-sort

--- yash/Sorting/sort.cpp
+++ yash/Sorting/sort.cpp
@@ -115,20 +115,50 @@ void mergeSort(vector<int>& arr, int n) {
     mergeSec(arr, 0, n-1);
 }
 
+// Heap Sort
+void heapify(vector<int>& arr, int n, int i) {
+    int largest = i;
+    int left = 2*i+1;
+    int right = 2*i+2;
+
+    if(left < n && arr[left] > arr[largest])
+        largest = left;
+    if(right < n && arr[right] > arr[largest])
+        largest = right;
+    if(largest != i) {
+        swap(arr[i], arr[largest]);
+
+        heapify(arr, n, largest);  
+    }
+}
+
+void heapSort(vector<int>& arr, int n) {
+    for(int i = n/2  -1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+    for(int i = n-1; i >= 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr,i,0);
+    }
+}
+
 int main(){
     int n;
     cin>>n;
     int arr[n];
+    vector<int> arr1;
     for (int i = 0; i < n; i++) {
         cin>>arr[i];
     }
+    for (int i = 0; i < n; i++) {
+        cin>>arr1[i];
+    }
     // bubbleSort(arr,n);
     // insertionSort(arr,n);
-    quickSort(arr,0,n-1);
+    // quickSort(arr,0,n-1);
+    heapSort(arr1, n);
     for (int i = 0; i < n; i++) {
         cout<<arr[i];
     }
-    
-    
     return 0;
 }
