09977422f3e443e7312c02b1de9df59811abf371
Yash Mishra <yash01012002@gmail.com>
2023-08-16 00:00:14 +0530

    rotate-array

--- yash/Wells-Fargo/round-I.cpp
+++ yash/Wells-Fargo/round-I.cpp
@@ -43,7 +43,27 @@ vector<int> rotateArray(vector<int>arr, int k) {
     return arr;
 }
 
-
+// Next Permutation
+vector<int> nextPermutation(vector<int> &permutation, int n) {
+    int index = -1;
+    for(int i = n - 2; i >= 0; i--) {
+        if(permutation[i] < permutation[i + 1]) {
+            index = i;
+            break;
+        }
+    }
+    if(index == -1) {
+        reverse(permutation.begin(),permutation.end());
+    }
+    for(int i = n - 1; i > index; i--) {
+        if(permutation[i] > permutation[index]) {
+            swap(permutation[i],permutation[index]);
+        }
+        break;
+    }
+    reverse(permutation.begin() + index + 1, permutation.end());
+    return permutation;
+}
 
 int main() {
     int n,s;
