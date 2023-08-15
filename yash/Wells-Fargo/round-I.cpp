b95ed466f1af517ceef74186f09e223ebd8989b9
Yash Mishra <yash01012002@gmail.com>
2023-08-15 23:44:37 +0530

    rotate-array

--- yash/Wells-Fargo/round-I.cpp
+++ yash/Wells-Fargo/round-I.cpp
@@ -1,18 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> pairSum(vector<int> &arr, int s){
+// vector<vector<int>> pairSum(vector<int> &arr, int s){
+//     vector<vector<int>> answer;
+//     unordered_map<int, int> pair_map;
+//     for(int i = 0; i < arr.size(); i++) {
+//         if(pair_map.find(s - arr[i]) != pair_map.end()) {
+//             vector<int> temp = {arr[pair_map[s - arr[i]]], arr[i]};
+//             answer.push_back(temp);
+//         }
+//         pair_map[arr[i]] = i;
+//     }
+//     return answer;
+// }
+
+// Pair-Sum (array is given and K is given find all pairs whose sum is equal to K) 
+vector<vector<int>> pairSum(vector<int> &arr, int s) {
     vector<vector<int>> answer;
-    unordered_map<int, int> pair_map;
-    for(int i = 0; i < arr.size(); i++) {
-        if(pair_map.find(s - arr[i]) != pair_map.end()) {
-            vector<int> temp = {arr[pair_map[s - arr[i]]], arr[i]};
+    unordered_set<int> seen;
+    for (int i = 0; i < arr.size(); i++) {
+        if(seen.find(s - arr[i]) != seen.end()) {
+            vector<int> temp = {s - arr[i], arr[i]};
             answer.push_back(temp);
         }
-        pair_map[arr[i]] = i;
+        seen.insert(arr[i]);
     }
     return answer;
 }
+
+// Rotate array
+vector<int> rotateArray(vector<int>arr, int k) {
+    int p = 1;
+    int n = arr.size();
+    while (p <= k) {
+        int last = arr[0];
+        for (int i = 0; i < n; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[n - 1] = last;
+        p++;
+    }
+    return arr;
+}
+
+
 
 int main() {
     int n,s;
@@ -23,6 +54,8 @@ int main() {
         cin>>arr[i];
     }
     temp = pairSum(arr, s);
-    
+    for(const auto &p :temp) {
+        cout<<p[0]<<","<<p[1]<<endl;
+    }
     return 0;
 }
