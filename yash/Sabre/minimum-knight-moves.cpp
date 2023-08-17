10119273581830f90eaef46fac3f3fd68e8be6b7
Yash Mishra <yash01012002@gmail.com>
2023-08-17 15:45:50 +0530

    minimum-knight-moves

--- yash/Sabre/minimum-knight-moves.cpp
+++ yash/Sabre/minimum-knight-moves.cpp
@@ -2,7 +2,31 @@
 using namespace std;
 
 int minKnightMoves(int x, int y) {
-    
+    x = abs(x);
+    y = abs(y);
+    vector<pair<int, int>> direc {{2,1},{-2,1},{2,-1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
+    pair<int, int> start {0,0};
+    int moves = 0;
+    queue<pair<int ,int>> q;
+    q.push(start);
+    set<pair<int, int>> visited;
+    while (!q.empty()) {
+        int q_size = q.size();
+        for(int i = 0; i < q_size; i++) {
+            pair<int, int> p = q.front();
+            visited.insert(p);
+            if(p.first == x && p.second == y) return moves;
+            q.pop();
+            for(auto d : direc) {
+                if(visited.find({p.first + d.first, p.second + d.second}) == visited.end() && p.first + d.first >= -2 && p.second + d.second >= -2) {
+                    q.push({p.first + d.first, p.second + d.second});
+                    visited.insert({p.first + d.first, p.second + d.second});
+                }
+            }
+        }
+        moves++;
+    }
+    return -1;
 }
 
 int main() {
--- yash/Wells-Fargo/round-I.cpp
+++ yash/Wells-Fargo/round-I.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 const int MOD = 1e9 + 7;
 
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left, *right;
+    TreeNode() : data(0), left(NULL), right(NULL) {}
+    TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+
 // vector<vector<int>> pairSum(vector<int> &arr, int s){
 //     vector<vector<int>> answer;
 //     unordered_map<int, int> pair_map;
@@ -171,6 +181,44 @@ void mergeSec(vector<int>& arr, int low, int high) {
 void mergeSort(vector<int>& arr, int n) {
     mergeSec(arr, 0, n-1);
 }
+
+vector<vector<int>> getTreeTraversal(TreeNode *root){
+    vector<vector<int>> answer;
+    stack<pair<TreeNode*, int>> st;
+    st.push({root, 1});
+    vector<int> pre, in, post;
+    if(root == NULL) return;
+    while(!st.empty()) {
+        auto it = st.top();
+        st.pop();
+
+        if(it.second == 1) {
+            pre.push_back(it.first -> data);
+            it.second++;
+            st.push(it);
+
+            if(it.first -> left) {
+                st.push({it.first -> left, 1});
+            }
+        } else if(it.second == 2) {
+            in.push_back(it.first -> data);
+            it.second++;
+            st.push(it);
+
+            if(it.first -> right) {
+                st.push({it.first -> right, 1});
+            }
+        } else {
+            post.push_back(it.first -> data);
+        }
+    }
+    answer.push_back(in);
+    answer.push_back(pre);
+    answer.push_back(post);
+    return answer;
+}
+
+
 
 int main() {
     int n,s;
