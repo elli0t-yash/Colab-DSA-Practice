59c2d2fa9e68d9e3ecc8a9dce537889a2c445f8c
Yash Mishra <yash01012002@gmail.com>
2023-09-06 01:29:09 +0530

    palindrome-substring

--- yash/Dp/practice.cpp
+++ yash/Dp/practice.cpp
@@ -42,6 +42,35 @@ int longestCommonSubstring(string text1, string text2) {
     return ans;
 }
 
+vector<vector<string>> partition(string s) {
+    vector<vector<string>> res;
+    vector<string> path;
+    substringCheck(0, s, path, res);
+}
+
+void substringCheck(int index, string s, vector<string>& path, vector<vector<string>>& res) {
+    if(index == s.size()) {
+        res.push_back(path);
+        return;
+    } 
+    for(int i = index; i < s.size(); i++) {
+        if(isPalindrome(s, index, i)) {
+            path.push_back(s.substr(index, i - index + 1));
+            substringCheck(i+1, s, path, res);
+            path.pop_back();
+        }
+    }
+}
+
+bool isPalindrome(string s, int start, int end) {
+    while(start <= end) {
+        if(s[start++] != s[end--]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
 
     return 0;
