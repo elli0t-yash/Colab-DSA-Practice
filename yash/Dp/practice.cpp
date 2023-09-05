01856f4e1ab5714d3e8e7919d0c1581d82a757fb
Yash Mishra <yash01012002@gmail.com>
2023-09-06 00:40:58 +0530

    lcs

--- yash/Dp/practice.cpp
+++ yash/Dp/practice.cpp
@@ -21,6 +21,27 @@ int longestCommonSubsequence(string text1, string text2) {
 }
 
 
+int longestCommonSubstring(string text1, string text2) {
+    int n = text1.size();        
+    int m = text2.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+    int ans = 0;
+
+    for(int i = n - 1; i >=0; i--) {
+        for(int j = m - 1; j >= 0; j--) {
+            if(text1[i] == text2[j]) {
+                dp[i][j] = 1 + dp[i+1][j+1];
+                ans = max(ans,dp[i][j]);
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }       
+
+    return ans;
+}
+
 int main() {
 
     return 0;
