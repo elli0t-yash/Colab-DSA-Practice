cece7b5a85df873e9a35d24638e367ca64949705
Yash Mishra <yash01012002@gmail.com>
2023-09-06 08:04:04 +0530

    mcm

--- yash/Dp/practice.cpp
+++ yash/Dp/practice.cpp
@@ -71,6 +71,91 @@ bool isPalindrome(string s, int start, int end) {
     return true;
 }
 
+//using dp (memoization)
+int no_of_partition(int index, int n, string& str, vector<int>& dp) {
+    if(index == n) return 0;
+    if(dp[index] != -1) return dp[index];
+    int minCost = INT_MAX;
+    for(int j = index; j < n; j++) {
+        if(isPalindrome(str, index, j)) {
+            int cost = 1 + no_of_partition(j+1, n, str, dp);
+            minCost = min(minCost, cost);
+        }
+    }
+    return dp[index] = minCost;
+}
+
+
+int palindromicPartition(string str) {
+    int n = str.size();
+    vector<int> dp(n, -1);
+    return no_of_partition(0, n, str, dp) - 1;
+}
+
+
+bool isPalindrome(string s, int start, int end) {
+    while(start <= end) {
+        if(s[start++] != s[end--]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// using tabular dp
+int palindromicPartition(string str) {
+    int n = str.size();
+    vector<int> dp(n + 1, 0);
+    dp[n] = 0;
+    for(int i = n-1; i >= 0; i--) {
+        int minCost = INT_MAX;
+        for(int j = i; j < n; j++) {
+        if(isPalindrome(str, i, j)) {
+            int cost = 1 + dp[j+1];
+            minCost = min(minCost, cost);
+        }
+    }
+     dp[i] = minCost;
+    }
+    return dp[0] - 1;
+}
+
+// MCM
+int no_of_multiplication(int i, int j, vector<int>& arr, vector<vector<int>>& dp) {
+    if(i == j) return 0;
+    if(dp[i][j] != -1) return dp[i][j];
+    int mini = 1e9;
+    for(int k = i; k < j; k++) {
+        int steps = arr[i-1] * arr[k] * arr[j] + no_of_multiplication(i, k, arr, dp) + no_of_multiplication(k+1, j, arr, dp);
+        if(steps < mini) mini = steps;
+    }
+    return dp[i][j] = mini;
+}
+
+
+int matrixMultiplication(vector<int> &arr, int N) {
+    vector<vector<int>> dp(N, vector<int>(N, -1));
+    return no_of_multiplication(1, N-1, arr, dp);
+}
+
+//using tabular dp
+int matrixMultiplication(vector<int> &arr, int N) {
+    int dp[N][N];
+    for(int i = 0; i < N; i++) dp[i][i] = 0;
+    for(int i = N - 1; i >= 1; i--) {
+        for(int j = i + 1; j < N; j++) {
+            int mini = 1e9;
+        for(int k = i; k < j; k++) {
+            int steps = arr[i-1] * arr[k] * arr[j] + dp[i][k] + dp[k+1][j];
+            if(steps < mini) mini = steps;
+        }
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][N-1];
+}
+
 int main() {
 
     return 0;
